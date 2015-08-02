621cbc6bc189534cd41594aa2cac7fb41e7f290a
nick <w19900227@gmail.com>
2015-08-02 21:22:23 +0800

    [Modify]from import of sample

--- ex3/main2.py
+++ ex3/main2.py
@@ -1,8 +1,13 @@
 from main_dir2 import sample1
 from main_dir2.s2 import sample2
-from main_dir2.sample3 import class_test3
+from main_dir2 import sample3
+from main_dir2.sample4 import class_test4
 
+# func
 sample1.test1()
 sample2.test2()
-class_test3().test3()
+
+# class
+sample3.class_test3().test3()
+class_test4().test4()
 
---
+++ ex3/main_dir2/sample4.py
@@ -0,0 +1,3 @@
+class class_test4(object):
+	def test4(self):
+		print "main-2 test4"
